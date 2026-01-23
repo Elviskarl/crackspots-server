181a550282fddedfabd803479c88863c5c9e233d
elvis <elviskarl343@gmail.com>
2026-01-23 14:06:16 +0300

    improved validation against missing fields and image processing for storage within the image bucket and database

--- src/controllers/reports.ts
+++ src/controllers/reports.ts
@@ -1,5 +1,9 @@
 import type { Request, Response } from "express";
 import reportModel from "../model/reportSchema.ts";
+import { connectImageBucket } from "../server/connectImageBucket.ts";
+import { wrapBuffer } from "../middleware/wrapBuffer.ts";
+import type { Report } from "../types/index.ts";
+import { handleExifData } from "../utils/handleExifData.ts";
 
 export async function getAllReports(req: Request, res: Response) {
   try {
@@ -10,14 +14,41 @@ export async function getAllReports(req: Request, res: Response) {
   }
 }
 
-export async function createReport(req: Request, res: Response) {
+export async function createReport(
+  req: Request<{}, {}, Report>,
+  res: Response,
+) {
   try {
-    if (!req.body) {
-      return res
-        .status(400)
-        .json({ success: false, message: "No data provided" });
+    // Validate request
+    if (!req.is("multipart/form-data")) {
+      return res.status(415).json({
+        error: "Expected multipart/form-data",
+      });
     }
-    return res.json({ success: true, message: "Report received" });
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+    if (!req.body.coordinates || !req.body.severity) {
+      return res.status(422).json({ error: "Missing required fields" });
+    }
+
+    // Process and upload image to Cloudinary
+    const stream = wrapBuffer(req.file.buffer);
+    const uploadResult = await connectImageBucket(stream, req.body.severity);
+
+    // Save report to database
+    const { lat, long, dateTaken } = handleExifData(req.body.coordinates);
+    await reportModel.create({
+      location: {
+        type: "Point",
+        coordinates: [long, lat],
+      },
+      severity: req.body.severity,
+      cloudinary_url: uploadResult.secure_url,
+      cloudinary_public_id: uploadResult.public_id,
+      dateTaken,
+    });
+    return res.json({ success: true, message: "Report successfully created" });
   } catch (error) {
     console.error(error);
     return res
