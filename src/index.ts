4023bd227884ab3f986b980a29bad9122f8ae203
elvis <elviskarl343@gmail.com>
2026-02-17 14:41:22 +0300

    added security and rate limiting middleware with helmet, cors, and express-rate-limit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import expressRateLimit from "express-rate-limit";
 
 config();
 
@@ -10,8 +13,16 @@ import { initializeCloudinaryConfig } from "./server/connectImageBucket.ts";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  expressRateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }),
+);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 app.use("/api/v1", reportRoute);
 
 app.listen(PORT, async () => {
